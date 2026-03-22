53c24c1a0b956bd27f285a4b3a59acacb7560430
fangqimingCSDN <39023905+fangqimingCSDN@users.noreply.github.com>
2026-03-22 22:49:05 +0800

    Delete scripts directory

--- scripts/gen_arch.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-import pathlib
-OUT = pathlib.Path(r'd:/B-Work/PyCharm/2025/speech_proj/ARCHITECTURE.md')
-OUT.write_text('', encoding='utf-8')  # clear file
-print('cleared')
